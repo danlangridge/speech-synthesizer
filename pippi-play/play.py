ee273184f8cddfc88eacd03a7d79eff30c6dea5f
Dan <dlangridge@gmail.com>
2014-10-08 21:35:39 -0700

    Added cli for dsp arguments

--- pippi-play/play.py
+++ pippi-play/play.py
@@ -1,5 +1,20 @@
 from pippi import dsp
+import os
+import sys
 
-out = dsp.tone(dsp.stf(5), freq=220, amp=0.2)
-out = dsp.env(out, 'hann')
-dsp.write(out, 'test')
+
+def output( frequency, amplitude, wave, outputFile):
+	out = dsp.tone(dsp.stf(5), frequency, amplitude)
+	out = dsp.env(out, wave)
+	dsp.write(out, outputFile)
+
+
+def main():
+	if len(sys.argv) < 4:  
+		output(220, 0.5, 'hann', 'test')	
+	else: 
+		output(sys.argv[0], sys.argv[1], sys.argv[2]. sys.argv[3])
+
+
+if __name__ == "__main__":
+	main() 
