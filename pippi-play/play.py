6f96b5a223cfa924f6abbf7301dcb4b6da3f462d
Dan <dlangridge@gmail.com>
2014-10-16 22:16:36 -0700

    added Voice class

--- pippi-play/play.py
+++ pippi-play/play.py
@@ -3,16 +3,35 @@ from pippi import tune
 import os
 import sys
 
+class Voice:
+	tones = []
+	length = 10
+	sound
+		
+	def generateVoice(segmentLength, tonesList):
+		tones = tonesList	
+		length = segmentLength
+
+	def combineTones():
+		for i in tones:
+			sound = dsp.interleave(sound, i)
+		return		
+			
+	def output(filename):
+		dsp.write(sound, outputfile) 
+	
 
 
 # Voice attempt 1 (AKA two tones)
-def voiceAttempt1(wordLength, frequency, amplitude, symbolLength, outputfile):
-	vowelA = dsp.tone(dsp.stf(wordLength*1/symbolLength), frequency, amplitude) 
-	vowelA += dsp.tone(dsp.stf(wordLength*1/symbolLength), frequency, amplitude) 
-	vowelA += dsp.tone(dsp.stf(wordLength*1/symbolLength), frequency*0.8, amplitude) 
+def voiceAttempt1(wordLength, frequency, amplitude, outputfile):
+	vowelA = dsp.tone(dsp.stf(wordLength), frequency, amplitude) 
+	vowelA += dsp.tone(dsp.stf(wordLength), frequency, amplitude) 
+	vowelA += dsp.tone(dsp.stf(wordLength), frequency*0.8, amplitude) 
 	vowelA = dsp.env(vowelA, 'hann')
         dsp.write(vowelA, outputfile) 
 
+	print type(dsp.tone(dsp.stf(wordLength), frequency*0.8, amplitude)) 
+	print type(dsp.env(vowelA, 'hann'))
 
 def output(duration, frequency, amplitude, wave, outputFile):
 	out = dsp.tone(dsp.stf(duration), frequency, amplitude)
@@ -25,7 +44,7 @@ def main():
 		voiceAttempt1(5, 220, 0.5, 'voice')	
 	else: 
 		#output(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-		voiceAttempt1(int(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), sys.argv[5])
+		voiceAttempt1(int(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]), sys.argv[5])
 
 
 if __name__ == "__main__":
